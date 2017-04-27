e4c8b1199cbc2341037c42422bef89fd310bd880
Denis Popov <d.popov93@mail.ru>
2017-04-28 01:45:19 +0300

    added JavaDoc for Calculate class and package-info

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
 
+/**
+ * Class Класс для будущего калькулятора
+ * @author dpopov93
+ * @since 27.04.2017
+ * @version 1.0
+ */
+
 public class Calculate {
+
+	/**
+	 * Главная функция класса Calculate
+	 * @param args первый аргумент
+	 * @return результат выполнения программы
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
